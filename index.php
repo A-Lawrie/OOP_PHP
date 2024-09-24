462490c79c998d5d9fdbe79be6997dd0f3f6e98d
JCOCHARLIE <lawrieabuna@gmail.com>
2024-09-25 00:18:37 +0300

    Getters and setters

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP in PHP</title>
+    <?php include('class_lib.php') ;?>
+</head>
+<body>
+    <?php
+        $stefan = new person();
+        $jimmy = new person();
+
+        $stefan->set_name("Stefan Mischook");
+        $jimmy->set_name("Nick Waddles");
+
+        echo "Stefan's full name is ". $stefan->get_name(). "<br/>";
+        echo "Nicks's full name is ". $jimmy->get_name(). "<br/>";
+    ?>
+    
+</body>
+</html>
